9e89f73b693bdab188ef9d2e157c0abbba3a1df0
venkata rama krishna vommi <contactkrsna1@gmail.com>
2020-09-20 15:10:32 +0530

    Added logger to ContactPageController

--- sample2/src/main/java/com/krsna/sample2/controller/ContactPageController.java
+++ sample2/src/main/java/com/krsna/sample2/controller/ContactPageController.java
@@ -1,5 +1,7 @@
 package com.krsna.sample2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,11 +12,14 @@ import com.krsna.sample2.service.ContactService;
 @Controller
 public class ContactPageController {
 	
+	Logger logger = LoggerFactory.getLogger(ContactPageController.class);
+	
 	@Autowired
 	ContactService contactService;
 	
 	@GetMapping(value = "/contact")
 	public String showContactPage(ModelMap model) {
+		logger.info("ContactPageController.showContactPage()");
 		String email = contactService.getContactEmail();
 		model.put("EMAIL", email); //k,v
 		model.put("AGE", 24);
